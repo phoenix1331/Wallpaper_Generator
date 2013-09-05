56d099b3939c1d094847563e26dcf987194e58bf
Darren Williams <design@phoenix1331.co.uk>
2013-09-05 20:09:38 +0100

    Removed unused elements

--- index.php
+++ index.php
@@ -16,64 +16,50 @@ if(!empty($message)){
 }
 
 ?>
-<div id="content-custom">
 
 <form action="index.php" method="post">
- <style type="text/css">
-    label
-    {
-        font-size:20px; 
-        line-height:12px;
-        display:inline;
-		margin-right:20px;
-    } 
-</style>
- <div style="width:480px; margin:10px auto 10px auto; padding:30px;color:#555;">
-     
-      <input type="radio" style="width:20px; height:20px;" name="selection" value="1024x768" checked/> <label>1024 x 768</label>
-      <input type="radio" style="width:20px; height:20px;" name="selection" value="1280x800" /> <label>1280 x 800</label>
-      <input type="radio" style="width:20px; height:20px;" name="selection" value="1366x768" /> <label>1366 x 768</label>
-      </div>
-      <div align="center">
+
+      <input type="radio" name="selection" value="1024x768" checked/> <label>1024 x 768</label>
+      <input type="radio" name="selection" value="1280x800" /> <label>1280 x 800</label>
+      <input type="radio" name="selection" value="1366x768" /> <label>1366 x 768</label>
+
+<div class="center">
     <p>Name: <input type="text" name="text" value="Your Name Here" />
     <input type="hidden" name="submit" value="preview">
       <input type="submit" class="submit" value="Generate"/>
     </p>
   </div>
 </form>
-<p align="center">Type your name in the box above and press generate wallpaper.</p>
+<p>Type your name in the box above and press generate wallpaper.</p>
 
 
-  <div align="center">
-    <p>
-      <?php 
-
-$select = $_REQUEST["selection"];
-$text = $_REQUEST["text"];
-$return_url = 'return.php?selection=' . "" . $select . "" . '&text=' . "" .  $text;?>
+<?php 
+	$select = $_REQUEST["selection"];
+	$text = $_REQUEST["text"];
+	$return_url = 'return.php?selection=' . "" . $select . "" . '&text=' . "" .  $text;
+?>
 <?php 
 	$direct_text = 'The current selection is: <br />';
 	echo $direct_text.'Wallpaper Size = '.$select.'<br />Your Name = '.$text; 
 ?>
-     
-      
-<p align="center">Your wallpaper will look similar to the one below</p>
-<?php 
-//Change to switch
-if ($select == '1280x800')
-{ $frame = 'Wembley1280x800.png';
-$size = 'width="600" height="380"';
-}
-else if ($select == '1366x768')
-{ $frame = 'Wembley1366.png';
-$size = 'width="600" height="340"';
-}
-else
-{
-$frame = 'Wembley1024.png';
-$size = 'width="500" height="380"';
-}
+       
+<p>Your wallpaper will look similar to the one below</p>
 
+<?php 
+	//Change to switch
+	if ($select == '1280x800')
+	{ $frame = 'Wembley1280x800.png';
+	$size = 'width="600" height="380"';
+	}
+	else if ($select == '1366x768')
+	{ $frame = 'Wembley1366.png';
+	$size = 'width="600" height="340"';
+	}
+	else
+	{
+	$frame = 'Wembley1024.png';
+	$size = 'width="500" height="380"';
+	}
 ?>
 <img src="<?php echo $return_url; ?>" <?php echo $size; ?> border="1" />
 
@@ -86,8 +72,7 @@ $size = 'width="500" height="380"';
 <input type="hidden" name="submit" value="save">
 <input type="submit" value="Save this image">
 </form> 
-</div>
-</div><!--END CONTENT-->
+
 </div><!--END WRAP-->
 
 </body>
